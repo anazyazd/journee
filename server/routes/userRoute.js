4b8506c88dfaf50e617f5e14c66888719e211dbe
anazyazd <anazyazd@gmail.com>
2023-02-06 13:30:14 -0800

    'staging changes'

--- server/routes/userRoute.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-router.route('/register').post(userController.register);
-router.route('/user').post(userController.login);
